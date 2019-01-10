65d5d56ee81da702230e943498f80787528ac65b
Angelos Anagnostopoulos <aggelosanagnostopoulos1@gmail.com>
2019-01-10 22:31:37 +0200

    Add files via upload

--- Application.py
+++ Application.py
@@ -1,20 +1,42 @@
 ##################### Group project for python courses 2018 ####################
-"""A quick note about the code: In order to save ourselves from making multiple frames and switching back and forth
-we decided it was better to erase and recreate all the contents inside the menu canvas i.e. Buttons,Labels,Images 
-and then make the new menus from scratch."""
-############################## Essential stuff that we have to add ###############################
-"""What is left: Create the player pawns and make them move inside the canvas, add a background picture to level 1 and make level 2
-(same as level 1 but smaller more or less)."""
-########################## Optional but cool stuff to add to the application #########################
-""" 1) Add the questions for player count and names on a separete text box after clicking to play.
-2) Add animations and movement to the player's pawns. 3) Using threading in order to get tkinter and pygame to make out 
-a little bit and have some musicfunctioality that tkinter cannot offer."""
 
 ##################### Imports section ####################
 import tkinter as t
 import random
+import time
 from tkinter.simpledialog import askinteger
 from tkinter.simpledialog import askstring
+
+#################### Level 1 Toolkit ###################
+
+def YfromPosition(p):
+    return (p - 1) // 10
+
+def XfromPosition(p):
+    x = p % 10 
+    if x == 0: 
+        x = 10
+    if YfromPosition(p) % 2 == 0 : #even 
+        return x
+    else: #odd
+        return 11 - x
+
+#################### Level 2 Toolkit ###################
+
+def YfromPosition2(p):
+    return (p - 1) // 6
+
+def XfromPosition2(p):
+    x = p % 6
+    if x == 0: 
+        x = 6
+    if YfromPosition2(p) % 2 == 0 : #even 
+        if x == 0: 
+            return 6
+        else:
+            return x
+    else: #odd
+        return 7 - x
 
 #################### Classes Creation ###################
 
@@ -89,8 +111,7 @@ class Menu():
         self.playbutton.destroy()
         self.levelsbutton.destroy()
         self.exitbutton.destroy()
-        ''
-        
+
         
     def level_1(self):
         """This method kills the main menu and starts the game at level 1  by making an object of the Game class"""
@@ -135,7 +156,6 @@ class Level1():
         self.currentplayer = 0
         self.snl=t.Label(self.window,text="Snakes n Ladders",font=("Helvetica",22),fg="green")
         self.snl.place(x=700,y=30)
-        self.canvas2.create_line(600,0,600,1000)
         self.dicebutton=t.Button(self.window,text="Roll the dice",command=self.movement)
         self.dicebutton.place(x=780,y=110)
         self.eventlabel=t.Label(self.window,text="Roll the die to begin")
@@ -145,66 +165,54 @@ class Level1():
         """Returns a random number from 1 to 6 as to replicate a dice roll and simulates a turn between the players.
         It also keeps track of each player's position and moves them to the designated square if they landed on a snake
         of a ladder. Also takes care of moving each players' piece on the board accordingly."""
-        self.diceroll = random.randint(1,6)
-        self.eventlabel.configure(text="You rolled a {}".format(self.diceroll))
+        diceroll = random.randint(1,6)
+        self.eventlabel.configure(text="You rolled a {}".format(diceroll))
         self.eventlabel.place(x=780,y=80)
-        self.oldposition = self.names[self.currentplayer].position.get() 
-        self.names[self.currentplayer].position.set(self.names[self.currentplayer].position.get()+self.diceroll)
-        if  self.names[self.currentplayer].position.get() in self.snake_squares:
-            self.names[self.currentplayer].position.set(self.snake_squares.get(self.names[self.currentplayer].position.get()))
-        elif self.names[self.currentplayer].position.get() in self.ladder_squares:
-            self.names[self.currentplayer].position.set(self.ladder_squares.get(self.names[self.currentplayer].position.get()))
-        self.newposition = self.names[self.currentplayer].position.get()
-
-        ############################## NOT ODDS ############################
-        
-        #if (self.names[self.currentplayer].position.get() % 10) not in [1,3,5,7,9]:
-        #    self.canvas2.move(self.pawns[self.currentplayer],((self.newposition - self.oldposition) % 10) *60) , ((self.newposition - self.oldposition) // 10) *60)
-        
-        ############################## ODDS ################################
-        
-        #elif (self.names[self.currentplayer].position.get() % 10) in [1,3,5,7,9]:
-        #    self.canvas2.move(self.pawns[self.currentplayer],((10 - (self.newposition - self.oldposition) % 10) * 60), ((self.newposition - self.oldposition) // 10) *60)
-        
-        
-        if self.names[self.currentplayer].position.get() >= 100:
-            self.eventlabel.configure(text="{} won the game!!!".format(self.names[self.currentplayer]))
-            self.dicebutton.configure(command="")
-
+        oldposition = self.names[self.currentplayer].position.get()
+        newposition = oldposition + diceroll
+        # Check for collision against snake or ladder multiple times
+        while newposition in self.snake_squares or newposition in self.ladder_squares: 
+            if  newposition in self.snake_squares:
+                newposition = self.snake_squares[newposition]
+            elif newposition in self.ladder_squares:
+                newposition = self.ladder_squares[newposition]
+        # Check if the player Won.
+        # If he did, then reset his position to 100 to be still visible on the screen
+        if newposition >= 100:
+            newposition = 100
+            self.eventlabel.config(text = "{} Wins!".format(self.names[self.currentplayer].name))            
+        self.names[self.currentplayer].position.set(newposition)
+        # Calculate change in X and Y
+        dy = - (YfromPosition(newposition) - YfromPosition(oldposition))
+        dx = XfromPosition(newposition) - XfromPosition(oldposition)
+        # Move the coresponding circle 
+        for i in range (30):
+            time.sleep(0.025)
+            self.canvas2.move(self.names[self.currentplayer].pawn, 2*dx, 2*dy)
+            self.canvas2.update()
+        # Get the next player
         self.currentplayer += 1
         if self.currentplayer >= len(self.names):
             self.currentplayer = 0
+        self.canvas2.update()
 
     def setup_game(self):
-        """This method draws the players' pawns and presents
-         their names and positions on the right"""
+        """This method initialises the number and names of players,
+        draws their pawns and presents their names and positions on the right"""
+
         self.snake_squares = {17: 7, 62: 18, 64: 60, 87: 24, 93: 73, 95: 75, 99: 78}
         self.ladder_squares = {4: 14, 9: 31, 20: 38, 28: 84, 40: 59, 63: 81, 71: 91}
-        self.players= askinteger("snakes n ladders","How many players are there?")
-        #Draw the pawns of each player (They get deleted bottom up if less than 4 players are ingame)
-        self.p0 = self.canvas2.create_oval(5,548,25,568,fill=Level1.colors[0])
-        self.p1 = self.canvas2.create_oval(30,548,50,568,fill=Level1.colors[1])
-        self.p2 = self.canvas2.create_oval(5,573,25,593,fill=Level1.colors[2])
-        self.p3 = self.canvas2.create_oval(30,573,50,593,fill=Level1.colors[3])
-        self.pawns = []
-        Tip 
-        
-        self.pawns.append(self.p0)
-        self.pawns.append(self.p1)
-        self.pawns.append(self.p2)
-        self.pawns.append(self.p3)
-        if self.players>4 or self.players<1:
-            raise Exception("Player count is between 2 and 4")
-        if self.players == 2:
-            self.canvas2.delete(self.p2,self.p3)
-        elif self.players == 3:
-            self.canvas2.delete(self.p3)
-        if self.players>4 or self.players<1:
-            raise Exception("Player count is between 2 and 4")
+        self.players= askinteger("Snakes n Ladders","How many players are there?")
+        starting_locations = [(5,548,25,568), (30,548, 50,568), (5,573,25,593), (30,573, 50, 593)]
         self.names = []
         for i in range(0,self.players):
-            name = askstring("snakes n ladders","What is the name of player {}".format(i+1))
+            name = askstring("Snakes n Ladders","What is the name of player {}".format(i+1))
             player = Player(name)
+            player.pawn = self.canvas2.create_oval(starting_locations[i][0],
+                                                   starting_locations[i][1],
+                                                   starting_locations[i][2],
+                                                   starting_locations[i][3],
+                                                   fill = Level1.colors[i])
             self.names.append(player)
             playernames=t.Label(self.window,text="{0} is at square:".format(self.names[i].name),fg=Level1.colors[i])
             playernames.place(x=700,y=i*100+200)
@@ -237,68 +245,57 @@ class Level2():
         """Returns a random number from 1 to 6 as to replicate a dice roll and simulates a turn between the players.
         It also keeps track of each player's position and moves them to the designated square if they landed on a snake
         of a ladder. Also takes care of moving each players' piece on the board accordingly."""
-        self.diceroll = random.randint(1,6)
-        self.eventlabel.configure(text="You rolled a {}".format(self.diceroll))
+        diceroll = random.randint(1,6)
+        self.eventlabel.configure(text="You rolled a {}".format(diceroll))
         self.eventlabel.place(x=680,y=80)
-        self.oldposition = self.names[self.currentplayer].position.get() 
-        self.names[self.currentplayer].position.set(self.names[self.currentplayer].position.get()+self.diceroll)
-        if  self.names[self.currentplayer].position.get() in self.snake_squares:
-            self.names[self.currentplayer].position.set(self.snake_squares.get(self.names[self.currentplayer].position.get()))
-        elif self.names[self.currentplayer].position.get() in self.ladder_squares:
-            self.names[self.currentplayer].position.set(self.ladder_squares.get(self.names[self.currentplayer].position.get()))
-        self.newposition = self.names[self.currentplayer].position.get()
-
-        ############################## NOT ODDS ############################
-        
-        #if (self.names[self.currentplayer].position.get() % 10) not in [1,3,5,7,9]:
-        #    self.canvas2.move(self.pawns[self.currentplayer],((self.newposition - self.oldposition) % 10) *60) , ((self.newposition - self.oldposition) // 10) *60)
-        
-        ############################## ODDS ################################
-        
-        #elif (self.names[self.currentplayer].position.get() % 10) in [1,3,5,7,9]:
-        #    self.canvas2.move(self.pawns[self.currentplayer],((10 - (self.newposition - self.oldposition) % 10) * 60), ((self.newposition - self.oldposition) // 10) *60)
-        
-        if self.names[self.currentplayer].position.get() >= 30:
-            self.eventlabel.configure(text="{0} won the game!!! Press the button to start over.".format(self.names[self.currentplayer]))
-            self.dicebutton.configure(command=Menu.level_2)
-
+        oldposition = self.names[self.currentplayer].position.get()
+        newposition = oldposition + diceroll
+        while newposition in self.snake_squares or newposition in self.ladder_squares: 
+            if  newposition in self.snake_squares:
+                newposition = self.snake_squares[newposition]
+            elif newposition in self.ladder_squares:
+                newposition = self.ladder_squares[newposition]
+        # Check if the player Won.
+        # If he did, then reset his position to 100 to be still visible on the screen
+        if newposition >= 30:
+            newposition = 30
+            self.eventlabel.config(text = "{} Wins!".format(self.names[self.currentplayer].name))
+        self.names[self.currentplayer].position.set(newposition)
+        # Calculate change in X and Y
+        dy = - (YfromPosition2(newposition) - YfromPosition2(oldposition))
+        dx = XfromPosition2(newposition) - XfromPosition2(oldposition)
+        # Move the coresponding circle 
+        for i in range (25):
+            time.sleep(0.025)
+            self.canvas2.move(self.names[self.currentplayer].pawn, dx * (20/6) , dy * 4)    
+            self.canvas2.update()
+        # Get the next player
         self.currentplayer += 1
         if self.currentplayer >= len(self.names):
             self.currentplayer = 0
+        self.canvas2.update()
 
     def setup_game(self):
-        """This method draws the players' pawns and presents
-         their names and positions on the right"""
+        """This method initialises the number and names of players,
+        draws their pawns and presents their names and positions on the right"""
         self.snake_squares = {27:1,21:9,19:7,17:4}
         self.ladder_squares = {3:22,5:8,11:26,20:29}
-        self.players= askinteger("snakes n ladders","How many players are there?")
-        #Draw the pawns of each player (They get deleted bottom up if less than 4 players are ingame)
-        self.p0 = self.canvas2.create_oval(5,405,40,440,fill=Level2.colors[0])
-        self.p1 = self.canvas2.create_oval(45,405,75,440,fill=Level2.colors[1])
-        self.p2 = self.canvas2.create_oval(5,455,40,490,fill=Level2.colors[2])
-        self.p3 = self.canvas2.create_oval(45,455,75,490,fill=Level2.colors[3])
-        self.pawns = []
-        self.pawns.append(self.p0)
-        self.pawns.append(self.p1)
-        self.pawns.append(self.p2)
-        self.pawns.append(self.p3)
-        if self.players>4 or self.players<1:
-            raise Exception("Player count is between 2 and 4")
-        if self.players == 2:
-            self.canvas2.delete(self.p2,self.p3)
-        elif self.players == 3:
-            self.canvas2.delete(self.p3)
-        if self.players>4 or self.players<1:
-            raise Exception("Player count is between 2 and 4")
+        self.players= askinteger("Snakes n Ladders","How many players are there?")
+        starting_locations = [(5,448,25,468), (30,448, 50,468), (5,473,25,493), (30,473, 50, 493)]
         self.names = []
         for i in range(0,self.players):
-            name = askstring("snakes n ladders","What is the name of player {}".format(i+1))
+            name = askstring("Snakes n Ladders","What is the name of player {}".format(i+1))
             player = Player(name)
+            player.pawn = self.canvas2.create_oval(starting_locations[i][0],
+                                                   starting_locations[i][1],
+                                                   starting_locations[i][2],
+                                                   starting_locations[i][3],
+                                                   fill = Level2.colors[i])
             self.names.append(player)
             playernames=t.Label(self.window,text="{0} is at square:".format(self.names[i].name),fg=Level1.colors[i])
-            playernames.place(x=650,y=i*100+150)
+            playernames.place(x=600,y=i*100+150)
             playerposition=t.Label(self.window,textvariable=self.names[i].position)
-            playerposition.place(x=750,y=i*100+150)
+            playerposition.place(x=700,y=i*100+150)
 
 def menu():
     menu = Menu()
